9a21f8d94280625294cb17030019e10749363ac5
Matej Kafka <git@a-n.cc>
2024-04-02 20:01:01 +0200

    CI: Add workflow to build remote repository in GitHub Pages

---
+++ .github/build-remote-repo.ps1
@@ -0,0 +1,37 @@
+﻿param([Parameter(Mandatory)]$RemoteRepoDir)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
+Import-Module Pog
+
+$null = mkdir -Force $RemoteRepoDir
+cd $RemoteRepoDir
+
+rm -Recurse *
+
+$Packages = [array][Pog.InternalState]::Repository.Enumerate()
+
+$VersionMap = [ordered]@{}
+$Packages | % {
+    $VersionMap[$_.PackageName] = @($_.EnumerateVersions() | % ToString)
+}
+# not really html, but whatever
+$VersionMap | ConvertTo-Json -Depth 100 -Compress > index.html
+
+# TODO: build in parallel
+$TmpPackage = New-PogImportedPackage _zip_export
+try {
+    $Packages | % {
+        $null = mkdir $_.PackageName
+        $VersionCounter = 0
+        $_.Enumerate() | % {
+            $_.ImportTo($TmpPackage)
+            Compress-Archive "$($TmpPackage.Path)\*" ".\$($_.PackageName)\$($_.Version).zip"
+            $VersionCounter++
+        }
+        Write-Host "Package '$($_.PackageName)': $VersionCounter versions"
+    }
+} finally {
+    rm -Force -Recurse $TmpPackage.Path
+}
